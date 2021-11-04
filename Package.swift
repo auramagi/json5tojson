b51e52a8a038c736bb899efab0657257170f56e3
Mikhail Apurin <mike.apurin@gmail.com>
2021-11-05 02:51:02 +0900

    Add product

--- Package.swift
+++ Package.swift
@@ -6,6 +6,9 @@ import PackageDescription
 let package = Package(
     name: "json5tojson",
     platforms: [.macOS(.v12)],
+    products: [
+        .executable(name: "json5tojson", targets: ["json5tojson"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1")
     ],
